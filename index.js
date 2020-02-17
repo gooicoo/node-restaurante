93b22d4434ca46700e8d5da98ea47368f4060183
Gooicoo <didaclopez93@gmail.com>
2020-02-17 17:17:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+// framework express
+var express = require('express');
+// bodyparser ens permet processar variables GET i POST
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+// per renderitzar les plantilles (render)
+app.set('view engine','ejs');
+
+var mongo = require('mongodb').MongoClient;
+var mongoClient;
+
+
+
+// connexió a mongo i start app
+var mongo = require('mongodb').MongoClient;
+require('dotenv').config();
+var mongoClient;
+// consts
+const PORT = process.env.PORT || 5000
+const user = encodeURIComponent( process.env.DBUSER );
+const pass = encodeURIComponent( process.env.DBPASS );
+var dbConStr = "mongodb+srv://"+user+":"+pass+"@cluster0-bmodt.mongodb.net";
+mongo.connect( dbConStr, function( err, _client ) {
+    // si no ens podem connectar, sortim
+    if( err ) throw err;
+    mongoClient = _client;
+    // si no hi ha cap error de connexió, engeguem el servidor
+    app.listen(PORT, function () {
+        console.log('Example app listening on http://localhost:'+PORT+' !');
+    });
+});
+
+
+
+
+
+app.post('/restaurantes', function (req, res) {
+  var nom = req.body.name;
+  var direccio = req.body.location;
+  var telefon = req.body.pnumber;
+  var cp= req.body.cp;
+
+  var restaurantObj = { name: nom, location: direccio, pnumber: telefon, cp: cp };
+  var db = mongoClient.db("RestaurantDB");
+  db.collection("Restaurant").insertOne(restaurantObj, function(err, result) {
+    if (err) {
+        res.render("result",{msg:"KO"});
+        return;
+    }else{
+        res.render("result",{msg:"OK"});
+    }
+
+  });
+
+});
+app.get('/restaurantes', function (req, res) {
+  res.render('restaurantes');
+});
+
+// view: llistat elements
+app.get('/', function (req, res) {
+    var db = mongoClient.db("RestaurantDB");
+    var opcions = {};
+    var query = {};
+    db.collection('Restaurant').find( query, opcions ).toArray(function( err, docs ) {
+        if( err ) {
+            res.render( 'result', {msg:"error a la query"} );
+            return;
+        }
+        res.render( 'restaurantes', {"restaurantes":docs} );
+    });
+});
